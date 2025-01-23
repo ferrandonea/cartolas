b6dae7ce7eca8bd191db52fd53befefd87d81613
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-23 16:38:38 -0300

    primera versión, formato inicial y primer script de cartolas básico

--- cartolas/download_cartolas.py
+++ cartolas/download_cartolas.py
@@ -1,41 +1,136 @@
 from datetime import date, datetime
 from captchapass import predict
+from playwright.sync_api import Page, sync_playwright
+from utiles.decorators import retry_function
+from utiles.file_tools import generate_hash_image_name
+from typing import Any
+from pathlib import Path
 
 DEFAULT_HEADLESS = True
-URL_CARTOLAS = ""
+URL_CARTOLAS = (
+    "https://www.cmfchile.cl/institucional/estadisticas/fondos_cartola_diaria.php"
+)
 VERBOSE = True
+TIMEOUT = 500_000
+
+# Carpeta de este módulo
+CURRENT_FOLDER = Path(__file__).parent
+
+# Este es la carpeta donde se guardan los archivos temporales
+TEMP_FOLDER_NAME = "temp"
+TEMP_FOLDER = CURRENT_FOLDER / TEMP_FOLDER_NAME
+TEMP_FILE_NAME = generate_hash_image_name()
+TEMP_FILE_PWD = TEMP_FOLDER / TEMP_FILE_NAME
+
+# Carpeta donde se guardan los errores
+ERROR_FOLDER_NAME = "errors"
+ERROR_FOLDER = CURRENT_FOLDER / ERROR_FOLDER_NAME
+
+# Carpeta donde se guardan los correctos
+CORRECT_FOLDER_NAME = "correct"
+CORRECT_FOLDER = CURRENT_FOLDER / CORRECT_FOLDER_NAME
+
+#Carpeta donde se guardan los txt de las cartolas
+CARTOLAS_FOLDER_NAME = "cartolas_txt"
+CARTOLAS_FOLDER = CURRENT_FOLDER / CARTOLAS_FOLDER_NAME
 
 def from_date_to_datetime(input_date: datetime | date) -> date:
-    """ Esto recibe una fecha, si es datetime da la fecha, si es date la deja igual"""
+    """Esto recibe una fecha, si es datetime da la fecha, si es date la deja igual"""
     if not isinstance(input_date, (datetime, date)):
         raise ValueError("El input debe ser de tipo datetime o date")
     return input_date.date() if isinstance(input_date, datetime) else input_date
 
+
 def format_date_cmf(input_date: date | datetime) -> str:
-    """ Formatea una fecha a string para la CMF"""
+    """Formatea una fecha a string para la CMF"""
     CMF_DATE_FORMAT = r"%d/%m/%Y"
     return from_date_to_datetime(input_date).strftime(CMF_DATE_FORMAT)
 
+
+def goto_with_retry(page: Page, url_str: str, timeout: int = TIMEOUT) -> Any:
+    return page.goto(url_str, timeout=timeout)
+
+
+@retry_function
 def download_cartolas(
     start_date: date | datetime,
     end_date: date | datetime,
     headless: bool = DEFAULT_HEADLESS,
     url: str = URL_CARTOLAS,
-    verbose:  bool = VERBOSE
+    verbose: bool = VERBOSE,
+    temp_file_path: Path = TEMP_FILE_PWD,
+    error_folder: Path = ERROR_FOLDER,
+    correct_folder: Path = CORRECT_FOLDER,
+    cartolas_txt_folder: Path = CARTOLAS_FOLDER,
 ):
-    """ Descarga cartolas desde la CMF en unas fechas determinadas"""
-    
-     # Define una función lambda para formatear las fechas según el formato CMF
-    format_date = lambda d: format_date_cmf(d)
+    """Descarga cartolas desde la CMF en unas fechas determinadas"""
+
     # Aplica la función de formato a las fechas de inicio y fin
-    start_date, end_date = map(format_date, [start_date, end_date])
-    
+    start_date, end_date = map(format_date_cmf, [start_date, end_date])
+
     if verbose:
-        print ("*" * 80)
-        print (f"Descargando cartolas desde {start_date} hasta {end_date}")
+        print("*" * 80)
+        print(f"Descargando cartolas desde {start_date} hasta {end_date}")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=headless)
+        page = browser.new_page()
+        #goto_with_retry(page, url)
+        page.goto(url, timeout=TIMEOUT)
+        captcha_img = page.query_selector("img#captcha_img")
+        src = captcha_img.get_attribute("src")
+        # Acá usamos JS para obtener una representación de la imagen en bytes
+        image_data = page.evaluate(
+            """async (url) => {
+            const response = await fetch(url);
+            const buffer = await response.arrayBuffer();
+            return Array.from(new Uint8Array(buffer));
+        }""",
+            src,
+        )
+        with open(temp_file_path, "wb") as temp_file:
+            temp_file.write(bytearray(image_data))
         
-    
+        prediction = predict(temp_file_path)
+        
+        if verbose: 
+            print (f"Predicción del captcha: {prediction}")
+        
+        if len(prediction) != 6:
+            temp_file_path.rename(error_folder / f"{prediction}.png")
+            raise ValueError("El captcha no tiene 6 caracteres")
+        
+        # Llenamos el formulario      
+        page.evaluate(
+            f"document.querySelector('#txt_inicio').value = '{start_date}';"
+        )
+        page.evaluate(
+            f"document.querySelector('#txt_termino').value = '{end_date}';"
+        )
+        page.get_by_label("Ingrese los caracteres de la").fill(prediction)        
+
+        try:
+            with page.expect_download() as download_info:
+                page.get_by_role("button", name="GENERAR ARCHIVO").click()
+                download = download_info.value
+                if download:
+                    temp_file_path.rename(correct_folder / f"{prediction}.png")
+                    download_path = cartolas_txt_folder / download.suggested_filename
+                    download.save_as(download_path)
+                    print (f"Archivo descargado como: {download_path}") if verbose else None 
+                    # ACA FALTA CHEQUEAR EL TAMAÑO
+        except Exception as e:
+            print ("Error en la descarga") if verbose else None
+            print (f"Detalles: {e}") if verbose else None
+            temp_file_path.rename(error_folder / f"{prediction}.png")
+            raise e        
+
+            
 if __name__ == "__main__":
     start_date = date(2021, 1, 1)
-    end_date = date(2021, 12, 31)
-    download_cartolas(start_date, end_date)
+    end_date = date(2021, 1, 22)
+    import time
+    for _ in range(100):
+        
+        download_cartolas(start_date, end_date, headless=True)
+        time.sleep(10)
---
+++ utiles/decorators.py
@@ -0,0 +1,28 @@
+""" Decoradores """
+from typing import Callable, TypeVar
+import time
+
+# Tipo de dato genérico
+T = TypeVar("T")
+
+
+def retry_function(
+    func: Callable[..., T], max_attempts: int = 10, delay: int = 5
+) -> Callable[..., T]:
+    """Decorador que intenta ejecutar una función varias veces si hay una excepción"""
+
+    def wrapper(*args, **kwargs) -> T:
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                attempts += 1
+                print(f"Error en {func.__name__}: {e}")
+                print(f"Intento {attempts}/{max_attempts}. Esperando {delay} segundos")
+                time.sleep(delay)
+        raise Exception(
+            f"No se pudo ejecutar {func.__name__} después de {max_attempts} intentos"
+        )
+
+    return wrapper
