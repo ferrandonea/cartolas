16d2fff8a42996f7cce827f81f36aecae60da68b
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-23 14:08:17 -0300

    nueva rama de cero

--- config/__init__.py
+++
@@ -1,4 +0,0 @@
-DEFAULT_HEADLESS = True
-URL_CARTOLAS = (
-    "https://www.cmfchile.cl/institucional/estadisticas/fondos_cartola_diaria.php"
-)
--- main.py
+++
@@ -1,7 +0,0 @@
-from cartolas.cartolas import download_cartola
-from datetime import date
-
-fecha_inicio = date(2024, 1, 1)
-fecha_fin = date(2023, 1, 1)
-
-download_cartola(fecha_inicio, fecha_fin, verbose=True)
