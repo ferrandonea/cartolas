b29f4db4d5fa4d70caf72e3fe872888c41b56153
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-25 10:19:31 -0300

    exp retry

--- utiles/decorators.py
+++ utiles/decorators.py
@@ -26,3 +26,25 @@ def retry_function(
         )
 
     return wrapper
+
+def exp_retry_function(
+    func: Callable[..., T], max_attempts: int = 12
+) -> Callable[..., T]:
+    """Decorador que intenta ejecutar una función varias veces si hay una excepción
+    con un crecimiento exponencial en el delay"""
+
+    def wrapper(*args, **kwargs) -> T:
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                attempts += 1
+                print(f"Error en {func.__name__}: {e}")
+                print(f"Intento {attempts}/{max_attempts}. Esperando {pow(2, attempts)} segundos")
+                time.sleep(pow(2, attempts))
+        raise Exception(
+            f"No se pudo ejecutar {func.__name__} después de {max_attempts} intentos"
+        )
+
+    return wrapper
