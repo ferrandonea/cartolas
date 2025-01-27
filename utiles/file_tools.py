7f995b8bf02aa39c81d6b7e29a37cc33f2dcfbf2
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-27 19:28:50 -0300

    borrado de errores

--- utiles/file_tools.py
+++ utiles/file_tools.py
@@ -3,11 +3,15 @@
 import hashlib
 import random
 import string
+from pathlib import Path
 
-HASH_LENGHT = 12
+HASH_LENGTH = 12
+# Tamaño mínimo de las cartolas
+MIN_FILE_SIZE = 1000  # 1 KB
+WILDCARD_CARTOLAS_TXT = "ffmm*.txt"
 
 
-def generate_hash_name(length=HASH_LENGHT):
+def generate_hash_name(length=HASH_LENGTH):
     # Genera una cadena aleatoria
     random_string = "".join(
         random.choices(string.ascii_letters + string.digits, k=length)
@@ -18,10 +22,61 @@ def generate_hash_name(length=HASH_LENGHT):
     return hash_object.hexdigest()[:length]
 
 
-def generate_hash_image_name(lenght=HASH_LENGHT):
+def generate_hash_image_name(lenght=HASH_LENGTH):
     return generate_hash_name(length=lenght) + ".png"
 
 
+# Este import debe estar acá sino hay un error de importación circular
+# El comentario de noqa es para que no huevee con el orden de los imports ruff
+from cartolas.config import CARTOLAS_FOLDER # noqa: E402
+
+
+def clean_txt_folder(
+    folder: str | Path = CARTOLAS_FOLDER,
+    wildcard: str = WILDCARD_CARTOLAS_TXT,
+    delete_all: bool = False,
+    min_file_size: int = MIN_FILE_SIZE,
+) -> None | bool:
+    """
+    Elimina archivos de una carpeta basándose en su tamaño o elimina todos los archivos.
+
+    Args:
+        folder (str | Path): Ruta de la carpeta donde buscar los archivos.
+                             Por defecto, CURRENT_FOLDER/DOWNLOAD_FOLDER.
+        wildcard (str): Patrón de los archivos a procesar. Por defecto, "ffmm*.txt".
+        delete_all (bool): Si es True, elimina todos los archivos que coincidan con el patrón.
+                           Si es False, solo elimina archivos menores a min_file_size.
+        min_file_size (int): Tamaño mínimo de archivo en bytes. Archivos menores a este
+                             tamaño serán eliminados si delete_all es False.
+
+    Returns:
+        None | bool: None cuando se eliminan archivos, o True/False si check_len está activado.
+
+    Note:
+        - Si delete_all es False, se asume que los archivos menores a min_file_size no tienen
+          información útil y son eliminados.
+        - La función imprime mensajes indicando qué archivos han sido eliminados.
+
+    Example:
+        >>> clean_txt_folder(delete_all=True)
+        Archivo ffmm_20230101 borrado
+        Archivo ffmm_20230102 borrado
+        >>> clean_txt_folder(check_len=True)
+        False
+    """
+    folder_path = Path(folder)
+
+    for file in folder_path.glob(wildcard):
+        if file.stat().st_size < min_file_size or delete_all:
+            file.unlink()
+            if delete_all:
+                print(f"Archivo {file.stem} borrado")
+            else:
+                print(
+                    f"Archivo {file.stem} borrado porque es menor a {min_file_size / 1000:.2f} KB"
+                )
+
+    return None
 
 
 if __name__ == "__main__":
@@ -30,3 +85,7 @@ if __name__ == "__main__":
 
     print(file_name)
     print(generate_hash_image_name())
+    clean_txt_folder()
+    print(CARTOLAS_FOLDER)
+    for archivo in CARTOLAS_FOLDER.glob(WILDCARD_CARTOLAS_TXT):
+        print(archivo.stat().st_size)
