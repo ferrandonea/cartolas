2c1201eaa0bab6b2869ad442e3397635ce91d344
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-27 20:54:42 -0300

    orden de constantes

--- cartolas/update.py
+++ cartolas/update.py
@@ -9,14 +9,8 @@ from .download import get_cartola_from_cmf
 from utiles.file_tools import clean_txt_folder
 
 
-# Este es la carpeta donde se guardan los archivos temporales
-PARQUET_FOLDER_NAME = "parquet"
-PARQUET_FOLDER = CURRENT_FOLDER / PARQUET_FOLDER_NAME
-PARQUET_FILE_NAME = "cartolas.parquet"
-PARQUET_FILE_PATH = PARQUET_FOLDER / PARQUET_FILE_NAME
-
-
 def first_run() -> None:
+    pass
 
 if __name__ == "__main__":
     pass
