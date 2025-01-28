8358345a96124e52b494550d803c745b99a6f783
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-28 18:07:15 -0300

    agregada unicidad a save_lazyframe

--- cartolas/save.py
+++ cartolas/save.py
@@ -2,8 +2,8 @@ import polars as pl
 from pathlib import Path
 
 
-def save_lazyframe_to_parquet(lazy_df: pl.LazyFrame, filename: str | Path):
+def save_lazyframe_to_parquet(lazy_df: pl.LazyFrame, filename: str | Path, unique: bool = True):
     """Graba lazyframe en parquet"""
-
+    lazy_df = lazy_df.unique() if unique else lazy_df
     lazy_df.collect().write_parquet(filename)
     print(f"Archivo parquet grabado con éxito en {filename}")
