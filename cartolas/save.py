b1ce2556189d0bef0060747c07eb0bea5c0ea041
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-01-27 22:12:02 -0300

    salida texto

--- cartolas/save.py
+++ cartolas/save.py
@@ -5,3 +5,4 @@ def save_lazyframe_to_parquet(lazy_df: pl.LazyFrame, filename: str | Path):
     """ Graba lazyframe en parquet"""
     
     lazy_df.collect().write_parquet(filename)
+    print (f"Archivo parquet grabado con éxito en {filename}")
