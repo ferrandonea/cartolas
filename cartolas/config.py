220656fc8d897e6ac30cd9732e03560e5ff72369
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-02-18 20:04:07 -0300

    folder elmer

--- cartolas/config.py
+++ cartolas/config.py
@@ -43,6 +43,10 @@ PARQUET_FILE_PATH = PARQUET_FOLDER / PARQUET_FILE_NAME
 PARQUET_FOLDER_YEAR_NAME = "yearly"
 PARQUET_FOLDER_YEAR = CURRENT_FOLDER / PARQUET_FOLDER_YEAR_NAME
 
+# Carpeta donde se guardan los json de El Mercurio
+ELMER_FOLDER_NAME = "elmer"
+ELMER_FOLDER = CURRENT_FOLDER / ELMER_FOLDER_NAME
+
 # El import es acá para evitar importaciones circulares con file_tools.py
 from utiles.file_tools import generate_hash_image_name  # noqa: E402
 
