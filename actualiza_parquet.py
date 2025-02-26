ff33c29bc8977e3422f7ef711dbeae130110864c
Francisco Javier Errandonea Terán <ferrando@gmail.com>
2025-02-26 14:06:58 -0300

    incluyo datos del central

--- actualiza_parquet.py
+++ actualiza_parquet.py
@@ -1,4 +1,6 @@
 from cartolas.update import update_parquet
+from eco.bcentral import update_bcch_parquet
 
 if __name__ == "__main__":
     update_parquet()
+    update_bcch_parquet()
--- actualiza_parquet_year.py
+++ actualiza_parquet_year.py
@@ -1,4 +1,6 @@
 from cartolas.update_by_year import update_parquet_by_year
+from eco.bcentral import update_bcch_parquet
 
 if __name__ == "__main__":
     update_parquet_by_year()
+    update_bcch_parquet()
